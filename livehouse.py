cf861547d92d6fc4eb6b30ba36e981cf9d0834c4
Daniel Lin <daniel@localhost>
2015-11-01 18:42:46 +0800

    first commit

---
+++ livehouse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import re
+
+from livestreamer.plugin import Plugin
+from livestreamer.plugin.api import http, validate
+from livestreamer.stream import HLSStream
+
+_url_re = re.compile(r"https://livehouse.in/channel/(.+)")
+
+class LiveHouse(Plugin):
+    @classmethod
+    def can_handle_url(cls, url):
+        match = _url_re.match(url)
+        return match
+
+    def _get_streams(self):
+        url_match = _url_re.match(self.url)
+        if url_match:
+            _id = url_match.group(1)
+            url = 'https://hi.cdn.livehouse.in/%s/video/playlist.m3u8' % (_id)
+            streams = HLSStream.parse_variant_playlist(self.session, url)
+            return streams
+        else:
+             raise StreamError("Error open playlist, maybe it's not live stream ")         
+            
+__plugin__ = LiveHouse
+
