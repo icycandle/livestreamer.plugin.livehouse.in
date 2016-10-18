9912636e0c7a6de7546c76fb544b731aefdec126
icycandle <icycandle@gmail.com>
2016-10-18 16:27:17 +0800

    Update install-livehouse-livestreamer-plugin.sh

--- install-livehouse-livestreamer-plugin.sh
+++ install-livehouse-livestreamer-plugin.sh
@@ -1,3 +1,10 @@
-echo $XDG_CONFIG_HOME
-echo "Hello World"
-cd '.config/livestreamer/plugins'
+if [ -z "$XDG_CONFIG_HOME" ]
+then
+echo "\$XDG_CONFIG_HOME is empty"
+$XDG_CONFIG_HOME='~/.config'
+else
+echo "\$XDG_CONFIG_HOME is NOT empty"
+fi
+
+plugin_dir = $XDG_CONFIG_HOME+'/livestreamer/plugins'
+mkdir -p $plugin_dir
