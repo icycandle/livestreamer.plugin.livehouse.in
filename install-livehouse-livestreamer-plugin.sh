b6f9f157dadbebd0fde8a9e3d95aa5820f16fcb1
icycandle <icycandle@gmail.com>
2016-10-18 17:07:49 +0800

    Update install-livehouse-livestreamer-plugin.sh

--- install-livehouse-livestreamer-plugin.sh
+++ install-livehouse-livestreamer-plugin.sh
@@ -9,4 +9,5 @@ fi
 plugin_dir=$XDG_CONFIG_HOME'/livestreamer/plugins';
 mkdir -p $plugin_dir;
 cd $plugin_dir;
+echo "cd to $plugin_dir"
 curl -O 'https://raw.githubusercontent.com/icycandle/livestreamer.plugin.livehouse.in/master/livehouse.py'
